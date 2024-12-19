53e22bc9cbe3165b4a5e0cfabb19204920e80581
addy <addy@gmail.com>
2024-12-19 12:31:37 -0500

    added funcionalities to the home shoes and galleries

--- src/components/Home/ShoeDisplay/ShoeDisplay.tsx
+++ src/components/Home/ShoeDisplay/ShoeDisplay.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./ShoesDisplay.css";
-import shoes from "../../../assets/images/big-shoe1.png";
+// import shoes from "../../../assets/images/big-shoe1.png";
 
-const ShoeDisplay: React.FC = () => (
-  <div className="nike-shoes">
-    <img src={shoes} alt="Nike Shoes" />
-  </div>
-);
+interface ShoeDisplayProps {
+  selectedShoe: string;
+}
+
+const ShoeDisplay: React.FC<ShoeDisplayProps> = ({ selectedShoe }) => {
+  return (
+    <div className="nike-shoes">
+      <img src={selectedShoe} />
+    </div>
+  );
+};
 
 export default ShoeDisplay;
--- src/components/Home/ShoeGallery/ShoeGallery.tsx
+++ src/components/Home/ShoeGallery/ShoeGallery.tsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ShoesGallery.css";
 import shoes1 from "../../../assets/images/big-shoe1.png";
 import shoes2 from "../../../assets/images/big-shoe2.png";
 import shoes3 from "../../../assets/images/big-shoe3.png";
+import ShoeDisplay from "../ShoeDisplay/ShoeDisplay";
 
-const ShoeGallery: React.FC = () => (
-  <div className="all-shoes">
-    <div className="shoe-item">
-      <img src={shoes1} alt="Shoe 1" />
+const ShoeGallery: React.FC = () => {
+  const [mainShoes, setMainShoes] = useState(shoes1);
+  const shoes = [shoes1, shoes2, shoes3];
+  return (
+    <div className="all-shoes">
+      <ShoeDisplay selectedShoe={mainShoes} />
+      {shoes.map((shoe, index) => (
+        <div
+          key={index}
+          className="shoe-item"
+          onClick={() => setMainShoes(shoe)}
+        >
+          <img src={shoe} alt={`Shoe ${index + 1}`} />
+        </div>
+      ))}
     </div>
-    <div className="shoe-item">
-      <img src={shoes2} alt="Shoe 2" />
-    </div>
-    <div className="shoe-item">
-      <img src={shoes3} alt="Shoe 3" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default ShoeGallery;
