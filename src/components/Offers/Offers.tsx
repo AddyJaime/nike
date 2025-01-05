039ccd7d60a7e99a2329977837d1149ad71b8962
addy <addy@gmail.com>
2025-01-05 13:05:45 -0500

    this version is working of the proyect

--- src/components/Offers/Offers.tsx
+++ src/components/Offers/Offers.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "./Offers.css";
 import OffersImage from "../OffersImage/OffersImage";
+import Button from "../common/Button/Button";
 
 const Offers: React.FC = () => {
   return (
     <div className="offers">
       <OffersImage />
+      <div className="special-offer-container">
+        <h2>
+          <span>Special</span>
+          Offer
+        </h2>
+        <p>
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart. Navigate a realm of
+          possibilities designed to fulfill your unique desires, surpassing the
+          loftiest expectations. Your journey with us is nothing short of
+          exceptional.
+        </p>
+        <Button>Shop Now </Button>
+      </div>
     </div>
   );
 };
