670be18cb73fcb572d6d09e066c6941696ce9e9d
addy <addyjaime@gmail.com>
2025-01-27 20:11:24 -0500

    adding review component

---
+++ src/components/About/Reviews/Reviews.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Reviews = () => {
+  return (
+    <div>
+      <div>
+        <h1>What Our Customers say</h1>
+        <p>
+          Hear genuine stories from our satisfied customers about their
+          exceptional experiences with us.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
