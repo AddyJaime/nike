81dc2541b27c9a5d576d6734de40f1fd81545b40
addy <addyjaime@gmail.com>
2025-01-29 18:46:49 -0500

    sign up finished

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -5,7 +5,10 @@ const SignUp: React.FC = () => {
   return (
     <div className="sign-up-container">
       <div>
-        <p className="signup-paragram">Sign Up for Updates & Newsletter</p>
+        <p className="signup-paragram">
+          Sign Up for <span className="hight-light-update">Updates</span> &
+          Newsletter
+        </p>
       </div>
       <div>
         <input className="input" type="text" placeholder="subscribe@nike.com" />
