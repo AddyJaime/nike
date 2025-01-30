6a11c6c20a944295bcb53ffff8c04235d84d282a
addy <addyjaime@gmail.com>
2025-01-30 18:39:39 -0500

    styling logo now

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
       <Offers />
       <Reviews />
       <SignUp />
-      <Footer />
+      <Footer logoSrc={logo} />
     </div>
   );
 };
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./Footer.css";
-const Footer: React.FC = () => {
-  return <div className="footer"></div>;
+
+interface FooterProps {
+  logoSrc: string;
+}
+
+const Footer: React.FC<FooterProps> = ({ logoSrc }) => {
+  return (
+    <div className="footer">
+      <div className="logo">
+        <img src={logoSrc} />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
